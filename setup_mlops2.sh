56478477d0470a18958550dae536473e4ad3c3fc
joethi <Jothi.Thondiraj@colorado.edu>
2024-04-09 13:53:09 -0600

    Adding setup.sh to automate the resource creation

---
+++ setup_mlops2.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/sh
+
+# Set the necessary variables
+RESOURCE_GROUP="rg-dp100-l-mlops2"
+RESOURCE_PROVIDER="Microsoft.MachineLearning"
+REGIONS=("eastus" "westus" "centralus")
+RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+WORKSPACE_NAME="mlw-dp100-l-mlops2"
+COMPUTE_INSTANCE="ci-joth2457-mlops2"
+COMPUTE_CLUSTER="aml-cluster"
+
+# Register the Azure Machine Learning resource provider in the subscription
+echo "Register the Machine Learning resource provider:"
+az provider register --namespace $RESOURCE_PROVIDER
+
+# Create the resource group and workspace and set to default
+echo "Create a resource group and set as default:"
+az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az configure --defaults group=$RESOURCE_GROUP
+
+echo "Create an Azure Machine Learning workspace:"
+az ml workspace create --name $WORKSPACE_NAME 
+az configure --defaults workspace=$WORKSPACE_NAME 
+
+# Create compute instance
+echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+
+# Create compute cluster
+echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+
+# Create data assets
+echo "Create training data asset:"
+az ml data create --type uri_folder --name "diabetes-dev-folder" --path ../experimentation/data
